9ec560b958c0ef1e666ee4df7d0140311dded113
Melvin DOUCET <doucet.m@sfeir.com>
2023-01-25 22:30:22 +0100

    Add cors for vite client

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors";
 import apiRouter from "./routes/api.router.js";
 import clientRouter from "./routes/client.router.js";
 
 const app = express();
 const port = process.env.PORT ?? 5000;
 
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
 app.use(express.static("client"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
