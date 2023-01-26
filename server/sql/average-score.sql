e7a937d87f7f519c1bbd4e34123855890fd6de6b
Melvin DOUCET <doucet.m@sfeir.com>
2023-01-26 12:12:05 +0100

    Make project frontend only

--- server/sql/average-score.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  AVG(roster.result) AS average
-FROM
-  roster
-  JOIN player ON player.id = roster.playerId
-WHERE
-  player.id = ?
--- server/src/controllers/user.controller.ts
+++
@@ -1,6 +0,0 @@
-import Controller from "../core/Controller.js";
-import User from "../models/user.model.js";
-
-export default {
-  getUser: Controller.getOne(User)
-};
