773a3bc2b377acebfa209f3f0d446dcb356f40bb
Melvin DOUCET <doucet.m@sfeir.com>
2023-01-27 18:58:50 +0100

    export everything

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,19 +1,32 @@
 const API_URL = "http://localhost:8080/api/v1";
-const season = 2023;
 const teamId = import.meta.env.VITE_THIONVILLE_ID;
+export const season = 2023;
 
-function getApiData<T>(path: `/${string}`, defaultValue: T) {
-  return async (): Promise<T> => {
-    try {
-      const response = await fetch(API_URL + path);
-      const data = await response.json() as T | null;
-      return data as T;
-    } catch (error) {
-      return defaultValue;
-    }
-  };
+export const urls = {
+  PLAYERS: "/players/all",
+  MATCHES: "/matches/all",
+  TEAMS: "/teams/all",
+  BOARD_INFO: "/board-info/all"
+} as const;
+
+export async function getApiData<T>(path: `/${string}`, defaultValue: T) {
+  try {
+    const response = await fetch(API_URL + path);
+    const data = await response.json() as T | null;
+    return data as T;
+  } catch (error) {
+    return defaultValue;
+  }
 }
 
-export const getPlayers = getApiData<Player[]>(`/players/all?team_id=${teamId}`, []);
-export const getTeams = getApiData<Team[]>(`/teams/all`, []);
-export const getMatchInfo = getApiData<MatchInfo[]>(`/matches/full-info?season=${season}&team_id=${teamId}`, []);
+export const getPlayers = () => {
+  return getApiData<Player[]>(`${urls.PLAYERS}?team_id=${teamId}`, []);
+};
+
+export const getTeams = () => {
+  return getApiData<Team[]>(urls.TEAMS, []);
+};
+
+export const getMatches = () => {
+  return getApiData<Match[]>(`${urls.MATCHES}?season=${season}&team_id=${teamId}`, []);
+};
