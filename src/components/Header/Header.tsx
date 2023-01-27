bb2a5a0d94010d683e42723475937d678feaad51
Melvin DOUCET <doucet.m@sfeir.com>
2023-01-27 19:47:28 +0100

    header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,19 +3,21 @@ import "./Header.scss";
 
 export default function Header() {
   return (
-    <header>
-      <div className="header__left">
-        <h1>Thionville Échecs — Équipes N3</h1>
-      </div>
-      <div className="header__right">
-        <nav>
-          <ul>
-            <li><Link href="/joueurs">Joueurs</Link></li>
-            <li><Link href="/equipes">Équipes</Link></li>
-            <li><Link href="/matchs">Matchs</Link></li>
-          </ul>
-        </nav>
-      </div>
+    <header className="container-xxl bg-primary text-white px-3 py-2">
+      <article className="row">
+        <div className="col col-lg-auto">
+          <h1 className="fs-3 m-0 p-0">Thionville Échecs —&nbsp;Équipes N3</h1>
+        </div>
+        <div className="col">
+          <nav className="h-100 d-flex justify-content-end align-items-center">
+            <ul className="d-flex gap-3 m-0">
+              <li><Link href="/joueurs">Joueurs</Link></li>
+              <li><Link href="/equipes">Équipes</Link></li>
+              <li><Link href="/matchs">Matchs</Link></li>
+            </ul>
+          </nav>
+        </div>
+      </article>
     </header>
   );
 }
